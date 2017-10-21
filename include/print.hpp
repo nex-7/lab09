7abac81381c1ced2fe338373f433b53307077f5b
nex-7 <alex-malina_47@inbox.ru>
2017-10-21 21:17:05 +0300

    added tests

--- include/print.hpp
+++ include/print.hpp
@@ -1,25 +1,6 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-/*!
-    \brief Функция копирования текстовой строки
-    \param[in] text Копирование текстовой строки
-    \param[out] out Вывод текстовой строки на экран для контроля
-    
-    Код функции выглядит следующим образом:
-    \code
-        void print(const std::string& text, std::ostream& out = std::cout);
-    \endcode
-*/
+
 void print(const std::string& text, std::ostream& out = std::cout);
-/*!
-    \brief Функция копирования текстовой строки
-    \param[in] text Копирование текстовой строки
-    \param[out] out Вывод текстовой строки на экран для контроля
-    
-    Код функции выглядит следующим образом:
-    \code
-        void print(const std::string& text, std::ostream& out = std::cout);
-    \endcode
-*/
 void print(const std::string& text, std::ofstream& out);
