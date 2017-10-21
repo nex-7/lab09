bdda29f8dbf657c7529ef85a5443667b15e67b47
nex-7 <alex-malina_47@inbox.ru>
2017-10-21 21:40:15 +0300

    added doxygen.conf

--- include/print.hpp
+++ include/print.hpp
@@ -1,6 +1,23 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-
+/*!
+    \brief
+    \param[in] Копирование текстовой строки
+    \param[out] out Вывод текстово строки на экран для контроля
+    Код функции выглядит следующим образом:
+    \code
+        void print(const std::string& text, std::ostream& out = std::cout);
+    \endcode
+*/
 void print(const std::string& text, std::ostream& out = std::cout);
+/*!
+    \brief
+    \param[in] Копирование текстовой строки
+    \param[out] out Вывод текстово строки на экран для контроля
+    Код функции выглядит следующим образом:
+    \code
+        void print(const std::string& text, std::ofstream& out);
+    \endcode
+*/
 void print(const std::string& text, std::ofstream& out);
